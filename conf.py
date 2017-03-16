47967de62ac79249c737a55d569e0b172f3f509f
IzChrisHodges <chris.hodges@izenda.com>
2017-03-16 17:42:00 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -96,6 +96,10 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 #
 # html_theme_options = {}
 
+# A shorter title for the navigation bar.  Default is the same as html_title.
+#
+html_short_title = ''
+
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #       
